93bcf505d1fe38eb10ea682b88863ba749f148bf
codyhalstead <codyhalstead@hotmail.com>
2025-07-28 14:56:57 -0400

    Added thumbnail image, assets used pdf, and video demo to Git Page

--- Assets/Scripts/Stage1/Enemies/Destroyer.cs
+++ Assets/Scripts/Stage1/Enemies/Destroyer.cs
@@ -189,7 +189,7 @@ public class Destroyer : BaseEnemy
     private void logDeath()
     {
         GameDataManager gameDataManager = GameDataManager.GetInstance();
-        gameDataManager.CurrentData.destroyersKilled += 1;
+        gameDataManager.CurrentData.destroyersKilled += 100;
         if (gameDataManager.CurrentData.destroyersKilled == 100)
         {
             AchievementPopUpUI popup = GameObject.FindGameObjectWithTag("AchievementPopUp").GetComponent<AchievementPopUpUI>();
